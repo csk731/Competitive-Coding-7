9edd9df875106039fb8902a5df898f1b6b2fe48c
Chaitanya Allu <chaitanyaa1999@gmail.com>
2025-02-09 14:56:40 -0600

    Added Approach_2

--- LC526.java
+++ LC526.java
@@ -3,6 +3,7 @@
 // Did it run successfully on Leetcode? : Yes
 // where N is the input number
 
+// Approach 1
 public class LC526 {
     private void swap(int arr[], int i, int j){
         if(i==j) return;
@@ -36,6 +37,44 @@ public class LC526 {
             arr[i]=i+1;
         }
         int ans = permute(arr, 0, n);
+        return ans;
+    }
+}
+
+// Approach 2
+class LC526_1 {
+    int[] set;
+
+    private boolean isValid(int num1, int num2) {
+        if ((num1 % num2) == 0)
+            return true;
+        if ((num2 % num1) == 0)
+            return true;
+        return false;
+    }
+
+    private int permute(int i, int n) {
+        // BC
+        if (i == n + 1)
+            return 1;
+
+        int ans = 0;
+        // Logic
+        for (int pos = 1; pos <= n; pos++) {
+            if (!isValid(i, pos))
+                continue;
+            if (set[pos] == 1)
+                continue;
+            set[pos] = 1;
+            ans += permute(i + 1, n);
+            set[pos] = 0;
+        }
+        return ans;
+    }
+
+    public int countArrangement(int n) {
+        set = new int[n + 1];
+        int ans = permute(1, n);
         return ans;
     }
 }
